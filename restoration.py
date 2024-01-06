84a8918c9f3e39508301e29aa67dbc3cbbfae3bf
Cesar Francisco Chavez Maldonado <cesarchamal@gmail.com>
2024-01-06 06:02:59 +0100

    Update.

--- restoration.py
+++ restoration.py
@@ -1,6 +1,9 @@
 import sys
 sys.path.append('./CodeFormer/CodeFormer')
-sys.path.append('./RealESRGANDistortion')
+# sys.path.append('./RealESRGANDistortion')
+sys.path.append('/content/RealESRGANDistortion')
+# sys.path.append('./inswapper/CodeFormer/CodeFormer')
+# sys.path.append('./inswapper/RealESRGANDistortion')
 import realesrgan_utils
 
 
